4eb4fe4d4c042ce1fb2c17678e8c0ab2873ee382
Steven Acres <steven@swatteksystems.com>
2016-09-25 16:56:40 -0400

    Use hostname of rabbitmq server instead of ip (dynamic)

--- tests/sensu-client.pp
+++ tests/sensu-client.pp
@@ -1,6 +1,6 @@
 class { '::sensu':
   rabbitmq_password => 'correct-horse-battery-staple',
-  rabbitmq_host     => '192.168.100.180',
+  rabbitmq_host     => 'sensu-server',
   rabbitmq_vhost    => '/sensu',
   subscriptions     => 'all',
   client_address    => $::ipaddress_eth1,
